a7254343036fc015a464348ef6dceffb8a342481
KatieNFerg <ketchersidekatie@gmail.com>
2024-06-04 15:25:23 -0500

    work in progress
    Co-authored-by: mnstrpuss@hotmail.com <mnstrpuss@hotmail.com>

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace SpaDay6.Controllers;
+public class UserController : Controller
+{
+    public IActionResult Index()
+    {
+        
+        return View();
+    }
+   [HttpGet]
+    //[Route("/spa/user")]
+    public IActionResult Add(User newUser) //TAKE NOTE
+    {
+        return View("add");
+    }
+    [HttpPost]
+   public IActionResult SubmitAddUserForm(User newUser, string verify) 
+   {
+
+       if (newUser.Password != verify)
+       {
+        return View("add");
+       }
+       else
+       {
+        ViewBag.username = newUser.UserName;
+        
+        return View("Index");
+       }
+       
+    
+    //   add form submission handling code here
+        
+   }
+}
+
---
+++ SpaDay6/Models/User.cs
@@ -0,0 +1,42 @@
+﻿namespace SpaDay6;
+
+public class User
+{
+    public int Id { get; }
+
+    public int nextId = 1;
+    public string UserName { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public User() 
+    {
+        Id = nextId;
+        nextId++;
+    }
+
+    public User(string username, string email, string password) : this()
+    {
+        UserName = username;
+        Email = email;
+        Password = password;
+    }
+
+    public override string ToString()
+    {
+        return UserName;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is User @user && Id == @user.Id;
+        
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id);
+    }
+}
